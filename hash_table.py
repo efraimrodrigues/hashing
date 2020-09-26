9ae5f1bf6329c6c43f646bcf46b81dea51216c3f
Efraim Rodrigues <efraimnaassom@gmail.com>
2020-09-25 23:39:34 -0300

    Implements removing and searching methods.

--- hash_table.py
+++ hash_table.py
@@ -1,6 +1,7 @@
 #halving when 1/4 of the table is being used
 #universe = 18.446.744.073.709.551.615
 import numpy as np
+import math
 import random
 
 class hash_table:
@@ -11,6 +12,7 @@ class hash_table:
         self.c = c
         self.e = e
         self.table = []
+        self.n = 0
         self.tables = []
 
         table_size = 2**self.c
@@ -40,7 +42,43 @@ class hash_table:
     def __table(self, index, key):
         return self.tables[index][key]
 
+    def doubling(self):
+        old_table = self.table
+
+        self.n = 0
+        self.table = [None] * len(old_table) * 2
+
+        for element in old_table:
+            if element != None and not math.isnan(element):
+                self.insert(element)
+
+    def halving(self):
+        old_table = self.table
+
+        self.n = 0
+        self.table = [None] * math.ceil(len(old_table) / 2)
+
+        for element in old_table:
+            if element != None and not math.isnan(element):
+                self.insert(element)
+
     def hash(self, key):
+        t = None
+
+        bin_array = self.__bin(key)
+
+        for index, element in enumerate(bin_array):
+            if index > 0:
+                t = self.__xor(t, self.__table(index, self.__int(element)))
+            elif index == 0:
+                t = self.__int(element)
+
+        return t
+
+    def insert(self, key):
+        self.n = self.n + 1
+
+        h = 0
         t = None
         m = len(self.table)
 
@@ -49,28 +87,75 @@ class hash_table:
             self.table.append(key)
         else:
             #Checks if it needs doubling
-            if m + 1 >= (1 + self.e)*m:
+            if m < (1 + self.e)*self.n:
                 print("Doubling")
+                self.doubling()
+                m = len(self.table)
 
-            bin_array = self.__bin(key)
+            h = self.hash(key)
 
-            for index, element in enumerate(bin_array):
-                if index > 0:
-                    t = self.__xor(t, self.__table(index, self.__int(element)))
-                elif index == 0:
-                    t = self.__int(element)
-
-            h = t % m
+            t = h % m
             i = 1
-            
+
             #Linear probing
-            while self.table[h] != None:
-                h = (t + i) % m
+            while self.table[t] != None and not math.isnan(self.table[t]):
+                t = (h + i) % m
                 i = i + 1
 
-            self.table[h] = self.key
+            self.table[t] = key
 
-        return t
+        return h, t
+
+    def remove(self, key):
+        self.n = self.n - 1
+
+        h = self.hash(key)
+        m = len(self.table)
+
+        t = h % m
+        i = 1
+
+        #Linear probing
+        while self.table[t] != key and math.isnan(self.table[t]):
+            t = (h + i) % m
+            i = i + 1
+
+        if self.table[t] == key:
+            self.table[t] = float("NaN")
+        else:
+            return h, -1
+
+        #Checks if it needs halving
+        if self.n < m/4:
+            print("Halving.")
+            self.halving()
+
+        return h, t
+
+    def search(self, key):
+        h = self.hash(key)
+        m = len(self.table)
+
+        t = h % m
+        i = 1
+
+        #Linear probing
+        while self.table[t] != key and math.isnan(self.table[t]):
+            t = (h + i) % m
+            i = i + 1
+
+        if self.table[t] == key:
+            return h, t
+        else:
+            return h, -1
 
 hashing = hash_table(8, 1)
-print(hashing.hash(18446744073709551615))
+print(hashing.insert(5))
+
+print(hashing.insert(8))
+print(hashing.insert(5))
+
+print(hashing.remove(5))
+
+print(hashing.table)
+print(hashing.search(5))
